f3c10c7535eda0f45ce1a20d6b07b06e6631b333
DesmondMM <mutsakaradesmond@gmail.com>
2016-02-04 02:18:45 +0200

    Displayed Questions

--- src/home/home.ts
+++ src/home/home.ts
@@ -4,7 +4,10 @@ import { Http, Headers } from 'angular2/http';
 import { AuthHttp } from 'angular2-jwt';
 import { Router } from 'angular2/router';
 import {Response} from "angular2/http";
-
+import { contentHeaders } from '../common/headers';
+import { Router, RouterLink } from 'angular2/router';
+import {NgFor} from "angular2/common";
+import {Injectable} from "angular2/core";
 
 let template = require('./home.html');
 
@@ -16,7 +19,7 @@ let template = require('./home.html');
 @View({
   directives: [CORE_DIRECTIVES],
   template: template,
-
+  directives: [NgFor]
 })
 export class Home {
   jwt: string;
@@ -24,6 +27,7 @@ export class Home {
   response: string;
   api: string;
   myName: string;
+  question_list:Object;
 
 
   constructor(public router: Router, public http: Http, public authHttp: AuthHttp) {
@@ -31,12 +35,20 @@ export class Home {
     this.decodedJwt = this.jwt && window.jwt_decode(this.jwt);
     this.myName = "Desmond";
 
-    this.http.get('http://localhost:8000/question_list/')
-      .map(question_list => question_list.json.data);
-
-
+    var authHeader = new Headers();
+     if (this.jwt) {
+      authHeader.append('Authorization', 'JWT ' + this.jwt);
+    }
+    http.get('http://localhost:8000/question_list/', {
+      headers: authHeader
+    })
+      .map(res => res.json())
+      .subscribe(question_list => this.question_list = question_list);
   }
 
+  getClicked(id){
+    alert('Clicked ID' + id);
+  }
 
 
   logout() {
